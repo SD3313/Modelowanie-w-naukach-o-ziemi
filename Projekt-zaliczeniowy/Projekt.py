83f4a4c10ce21cd45e3d04c9efe7aeda3d6c9e2a
SD3313 <dziedzinaszymon1@gmail.com>
2025-06-06 16:47:13 +0200

    antarktyda

--- Projekt-zaliczeniowy/Projekt.py
+++ Projekt-zaliczeniowy/Projekt.py
@@ -1,5 +1,7 @@
+import numpy as np
 import pandas as pd
 import math
+import matplotlib.pyplot as plt
 
 # Wczytaj dane z pliku CSV
 df = pd.read_csv('daily_ice_edge.csv')
@@ -15,7 +17,20 @@ def wspolrzedne(kat,r):
     y = srodek[1] + r * math.sin(kat)
     return [x,y]
 
+wsp_row = []
 for x in range(1,len(df.columns)):
-    kat = x-1
-    r = df.iloc[0][x]
-    map(wspolrzedne(),(kat,r))
+    kat = (x-1)*2*np.pi/360
+    r = df[df.columns[x]].min()
+    wsp_row.append(wspolrzedne(kat,r))
+
+x_coords = [point[0] for point in wsp_row]
+y_coords = [point[1] for point in wsp_row]
+
+plt.figure(figsize=(10, 10))
+plt.plot(x_coords, y_coords, alpha=0.5)
+plt.grid(True)
+plt.axis('equal')
+plt.title('Visualization of Ice Edge Coordinates')
+plt.xlabel('X coordinate')
+plt.ylabel('Y coordinate')
+plt.show()
